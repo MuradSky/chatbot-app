a001affd2238f0bf0df6f48b85cf4454a641719b
Murad <skytechnic.music@gmail.com>
2024-10-25 15:04:17 +0300

    update

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export const api = {
     async postUserData(data) {
-        const res = await axios.post('/api', data);
+        const res = await axios.post('https://komusart.ru/workstatus', data);
         return res.data;
     }
 }
